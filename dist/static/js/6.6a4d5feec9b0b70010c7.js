b304bfcf48b2ad3d9febbb32c6c10f2c9f191ba6
John Perez <john.perez@kodakit.com>
2018-03-15 15:19:12 +0800

    Adding files.

---
+++ dist/static/js/6.6a4d5feec9b0b70010c7.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{BUYD:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n("XyMi"),o=Object(a.a)({name:"page-not-found",data:function(){return{}},methods:{},components:{}},function(){var e=this.$createElement,t=this._self._c||e;return t("v-layout",{attrs:{row:"row"}},[t("h3",[this._v("Oops! Page not yet available.")])])},[],!1,null,null,null);t.default=o.exports}});
+//# sourceMappingURL=6.6a4d5feec9b0b70010c7.js.map
